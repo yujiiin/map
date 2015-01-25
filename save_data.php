2c85d12e35cfbaa30d05a6f60b2bd48ac2b5f0df
yujiiin <root@ip-172-31-30-214.us-west-1.compute.internal>
2015-01-25 06:24:51 +0000

    done data insert function

--- save_data.php
+++ save_data.php
@@ -1,19 +1,35 @@
 <?php
 require("config.php");
-require("function.php");
 
-echo 'loaded';
-//Gets data from URL parameters
+//Connect to DB
+try{
+  $dbh =  new PDO(DSN, DB_USER, DB_PASSWORD);
+  $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}catch(PDOException $e){
+  echo $e->getMessage();
+  exit;
+}
+
+
+//$dbh = connectDb();
+//$dbh = new PDO(DNS, DB_USER, DB_PASSWORD);
+var_dump($dbh);
+
+
+//Gets data from URL s
 $name = $_GET['name']; 
 $memo = $_GET['memo'];
 $placeId = $_GET['placeId'];
 $lat = $_GET['lat'];
 $lng = $_GET['lng'];
 
-//Connect to DB
-$dbh = connectDb();
-echo 'Connected';
-console.log("db connected");
+/*
+var_dump($name);
+var_dump($memo);
+var_dump($placeId);
+var_dump($lat);
+var_dump($lng);
+*/
 
 $stmt = $dbh->prepare("insert into places (name,memo,placeId,lat,lng) values (:name,:memo,:placeId,:lat,:lng)");
 $stmt->bindParam(":name", $name);
@@ -23,5 +39,4 @@ $stmt->bindParam(":lat", $lat);
 $stmt->bindParam(":lng", $lng);
 $stmt->execute();
 
-echo 'Added';
-console.log("data added");
+?>
